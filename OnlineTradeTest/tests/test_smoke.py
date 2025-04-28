6409637b1d6dfbe940d0e9aa7c05c054986e5471
llvlvll <160230730+llvlvll@users.noreply.github.com>
2025-04-28 13:55:24 +0300

    Add files via upload

--- OnlineTradeTest/tests/test_smoke.py
+++ OnlineTradeTest/tests/test_smoke.py
@@ -14,17 +14,23 @@ from pages.order import OrderProduct
 from pages.filters.result_page_choose_product import Product
 
 
+
 def test_smoke_buy_product():
     options = webdriver.ChromeOptions()
-    # options.add_argument("--guest")
+    options.add_argument("--guest")
     options.add_experimental_option("detach", True)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("--disable-blink-features=AutomationControlled")
     g = Service()
     driver = webdriver.Chrome(options=options, service=g)
+
 
     print("Start Test")
 
     ctl = OpenCatalogue(driver) # открываем меню, и выбираем раздел электроника
     ctl.open_catalogue()
+    time.sleep(1)
 
     sm_gd = OpenSmartGadget(driver) # на странице электроники выбираем смартфоны и гаджеты
     sm_gd.open_smart_gadgets()
@@ -39,46 +45,41 @@ def test_smoke_buy_product():
     p.move()
     p.set_price()
 
-    memory = Memory(driver) # выбираем обьем оператвки и встроенной памяти
-    memory.move_to_memory() # перезод к фильтру памяти
+    memory = Memory(driver) # фильтр #3 выбираем обьем оператвки и встроенной памяти
+    memory.move_to_memory() # переход к фильтру памяти
     time.sleep(1)
     memory.click_ram() # выбираем размер встроенной
     time.sleep(1)
     memory.click_op_memory() # выбираем размер оперативной
     time.sleep(1)
 
-    result = Filter_Results(driver) # класс с методом, чтобы показывать результат после тех или иных фильтров
+    result = Filter_Results(driver) # класс с методом, чтобы показывать результат после выбора фильтров
     result.show_results() # после выбора всех фильтров показываем результат
 
     pr = Product(driver)
-    r = pr.price_cotalogue() #сохраняем в переменную цену из каталога
-    Name_catol = f'Смартфон {pr.name_cotalogue()}'
+    r = pr.price_cotalogue()
+    Name_catol = f'Смартфон {pr.name_cotalogue()}' #сохраняем в переменную - цену из каталога
     print(Name_catol)
     print(r)
     pr.choose() # выбираем продукт
 
     order_p = OrderProduct(driver)
-    order_p.order() # офрмляем, а так же выбираем, что оформим без регестрации
+    order_p.order() # офрмляем, а так же выбираем, что заказ оформим без регестрации
 
-    cr_pr = CartProduct(driver) #сверяем продукт
-    s = cr_pr.save_price_cart() # сохраняем цену из корзины в перменную
-    Name_cart = cr_pr.save_name_cart()
+    cr_pr = CartProduct(driver) #сверяем имя продукта
+    s = cr_pr.save_price_cart()
+    Name_cart = cr_pr.save_name_cart() # сохраняем цену из корзины в перменную
     cr_pr.cart_pr()
     print(Name_cart)
     print(s)
 
-    assert r == s # равниваем цену из каталога и корзины
+    assert r == s # сравниваем цену из каталога и корзины
     print(f'{r} = {s} Отлично!')
-
-    assert Name_cart == Name_catol
+    assert Name_cart == Name_catol # сверяем название из катлога и корзины
     print(f'{Name_cart} = {Name_catol} Отлично!!')
 
 
 
-
-    # //td[@class='multicart__item__description']/div[2]/a корзина
-    # (//div[@class='indexGoods__item__descriptionCover'])[2]/a каталог
-    # driver.quit()
 
 
 # //b[@class='nowrap'] 57 999
